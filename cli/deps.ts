7a5b90246a96096a6edc142f0bd2bfb9b6a1c52a
jw397 <jw397@126.com>
2024-10-28 10:56:17 +0800

    feat(cli): use jsr

--- cli/deps.ts
+++ cli/deps.ts
@@ -8,5 +8,5 @@ export * from "jsr:@cliffy/prompt@1.0.0-rc.7";
 export * from "jsr:@cliffy/ansi@1.0.0-rc.7/colors";
 
 // Other dependencies
-export * from "https://deno.land/x/zip@v1.2.2/mod.ts";
-export * from "https://deno.land/x/jsonc@1/main.ts";
+export * from "jsr:@fakoua/zip-ts@^1.3";
+export * from "npm:jsonc-parser@^3.3";
