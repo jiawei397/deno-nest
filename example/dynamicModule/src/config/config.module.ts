b80f550328687b4e2d857c6cf5e0a05c763321e2
jw397 <jw397@126.com>
2024-10-31 16:39:00 +0800

    docs: change task

--- example/dynamicModule/src/config/config.module.ts
+++ example/dynamicModule/src/config/config.module.ts
@@ -2,8 +2,8 @@ import { DynamicModule, Module } from "@nest/core";
 import { ConfigService } from "./config.service.ts";
 import { ConfigOptions, EnvConfig } from "./config.interface.ts";
 import { CONFIG_KEY } from "./config.constant.ts";
-import { load } from "std/dotenv/mod.ts";
-import { join } from "std/path/join.ts";
+import { load } from "@std/dotenv";
+import { join } from "@std/path/join";
 
 @Module({})
 export class ConfigModule {
